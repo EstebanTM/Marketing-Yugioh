f45b2e8be7ed4e540ba749843a1f71273acffa5c
EstebanTM <s18120211@alumnos.itsur.edu.mx>
2024-07-08 12:21:56 -0600

    cambio de prueba

--- gameplay.php
+++ gameplay.php
@@ -13,6 +13,7 @@ session_start();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+    
     <!-- Header -->
     <header class="header">
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
